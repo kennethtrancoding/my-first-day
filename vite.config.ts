2b36dc6eac2fac6177de2647053a7036f743309d
kennethtrancoding <kennethtrancoding@gmail.com>
2025-10-21 19:27:06 -0700

    corrected config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ command }) => ({
 	server: {
 		host: "::",
 		port: 8080,
@@ -14,5 +14,5 @@ export default defineConfig(({ mode }) => ({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-	base: "/my-first-day/",
+	base: command === "build" ? "/my-first-day/" : "/",
 }));
